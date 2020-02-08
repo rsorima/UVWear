8fa34c2828029af192832554e39b4da5f1ea7b03
mayflorlape <lapemayflor7@gmail.com>
2020-02-08 15:54:09 +0800

    sample

--- app/src/main/java/com/example/uvweariot/HeartFragment.java
+++ app/src/main/java/com/example/uvweariot/HeartFragment.java
@@ -15,6 +15,6 @@ public class HeartFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container,false);
+        return inflater.inflate(R.layout.fragment_heart, container,false);
     }
 }
